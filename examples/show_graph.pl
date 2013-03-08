0da742ad05018ab0d4da172024ea9070cb2d55b8
Gilberto Gonçalves <lursty@gmail.com>
2013-03-08 07:29:35 +0000

    Moved example files to examples/; Changed show_graph.pl to vspark.pl

---
+++ examples/show_graph.pl
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+
+use Term::Vspark qw{ show_graph };
+
+# VERSION
+# PODNAME: show_graph
+
+binmode STDOUT, ':encoding(UTF-8)';
+
+chomp( @ARGV = <STDIN> ) unless @ARGV;
+
+my @list = sort { $a <=> $b } @ARGV;
+my $columns = qx{tput cols};
+
+print show_graph(
+    'max'     => $list[-1],
+    'columns' => $columns - 1,
+    'values'  => \@ARGV,
+);

789484f391a226dc0ee1fd6db424a551ce1c4a13
Eric Johnson <eric.git@iijo.org>
2013-03-13 08:47:31 +0000

    rm 'k_values' param and replace with 'values' and 'labels' params

--- lib/Term/Vspark.pm
+++ lib/Term/Vspark.pm
@@ -7,7 +7,7 @@ use Carp qw{ croak };
 use utf8;
 
 use Sub::Exporter -setup => {
-    'exports' => [ 'show_bar', 'show_graph', 'show_labeled_graph', ],
+    'exports' => [ 'show_bar', 'show_graph', ],
 };
 
 our @ISA = qw();
@@ -28,49 +28,42 @@ sub show_bar {
     return $str;
 }
 
-# Helper method
 sub show_graph {
-    my %args    = @_;
+    my %args = @_;
+
+    croak 'values is not an ArrayRef' 
+        unless ref $args{'values'} eq 'ARRAY';
+    croak 'labels is not an ArrayRef' 
+        if $args{'labels'} && ref $args{'labels'} ne 'ARRAY';
 
     my $max     = $args{'max'}     || 1;
     my $columns = $args{'columns'} || 1;
-    my $values  = $args{'values'}  || [];
+    my @labels  = @{ $args{'labels'} || [] };
+    my @values  = @{ $args{'values'} };
 
-    my $str = q{};
-    for my $i ( @{ $values } ) {
-        $str .= sprintf( "%s\n", show_bar($i, $max, $columns) );
-    }
+    croak 'the number of labels and values must be equal' 
+        if $args{'labels'} && scalar @labels != scalar @values;
 
-    return $str;
-}
-
-# Helper method
-sub show_labeled_graph {
-    my %args     = @_;
-
-    my $max      = $args{'max'}     || 1;
-    my $columns  = $args{'columns'} || 1;
-
-    if ( ref $args{'k_values'} ne q{HASH} ) {
-        croak 'k_values is not an HASH';
-    }
-    my %k_values = %{ $args{'k_values'} };
-
-    my $label_width = max_label_width( keys %k_values );
+    my $label_width = max_label_width( @labels );
     my $bar_width   = $columns - $label_width - 2;
     my $str         = q{};
 
-    my $bar = q{};
-    for my $i ( keys %k_values ) {
-        $bar = show_bar($k_values{$i}, $max, $bar_width);
-        $str .= sprintf( '%' . $label_width . "s %s\n", $i, $bar );
+    for my $value (@values) {
+        my $label = shift @labels;
+        my $bar   = show_bar($value, $max, $bar_width);
+
+        $str .= sprintf('%' . $label_width . "s ", $label) if defined $label;
+        $str .= $bar . "\n";
     }
 
     return $str;
 }
 
 sub max_label_width {
-    my $max_width = (sort { $a <=> $b } map { length($_) } @_)[-1];
+    my @labels = @_;
+    return 0 if scalar @labels == 0;
+
+    my $max_width = (sort { $a <=> $b } map { length($_) } @labels)[-1];
     return $max_width + 1;
 }
 

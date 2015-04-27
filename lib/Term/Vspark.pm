bbad7866dba2e91be0332fa375701d4fde2f24d5
Eric Johnson <eric.git@iijo.org>
2015-04-26 20:45:42 -0500

    New tests, new fixes, new defaults, updated pod

--- lib/Term/Vspark.pm
+++ lib/Term/Vspark.pm
@@ -21,7 +21,11 @@ sub show_bar {
         ? ($char)
         : (qw{ ▏ ▎ ▍ ▌ ▋ ▊ ▉ █});
 
+    # calculate the length of the bar
     my $length = $value * $columns / $max; # length of the bar
+    $length = $columns if $length > $columns;
+
+    # empty $bar
     my $bar = '';
 
     # build integer portion of the bar
@@ -47,8 +51,8 @@ sub show_graph {
     croak 'labels is not an ArrayRef'
         if $args{'labels'} && ref $args{'labels'} ne 'ARRAY';
 
-    my $max     = $args{max}       || 1;
-    my $columns = $args{columns}   || 3;
+    my $max     = $args{max};
+    my $columns = _term_width($args{columns});
     my @labels  = @{ $args{labels} || [] };
     my @values  = @{ $args{values} };
     my $char    = $args{char};
@@ -56,8 +60,9 @@ sub show_graph {
     croak 'the number of labels and values must be equal'
         if $args{labels} && scalar @labels != scalar @values;
 
-    my $label_width = max_label_width(@labels);
-    my $bar_width   = $columns - $label_width - 2;
+    $max          //= _max_value(@values);
+    my $label_width = _max_label_width(@labels);
+    my $bar_width   = $columns - $label_width;
     my $graph       = q{};
 
     for my $value (@values) {
@@ -69,20 +74,50 @@ sub show_graph {
             char    => $char,
         );
 
-        $graph .= sprintf(' %' . $label_width . "s ", $label) if defined $label;
+        $graph .= sprintf("%${label_width}s", " $label ") if defined $label;
         $graph .= $bar . "\n";
     }
 
     return $graph;
 }
 
-sub max_label_width {
+sub _term_width {
+    my $columns = shift;
+    return $columns if $columns && $columns ne 'max';
+
+    require Term::ReadKey;
+    my ($cols) = Term::ReadKey::GetTerminalSize(*STDOUT);
+    return 80    if !$cols;
+    return $cols if  $columns eq 'max';
+    return 80    if $cols > 80;
+    return $cols;
+}
+
+#sub _term_width {
+#    eval { require 'sys/ioctl.ph'; };
+#    return if $@;
+#    return unless defined &TIOCGWINSZ;
+#    open my $tty, '+<', '/dev/tty' or return;
+#    my $winsize = '';
+#    return unless ioctl( $tty, &TIOCGWINSZ, $winsize );
+#    my $col = ( unpack( 'S4', $winsize ) )[1];
+#    return $col;
+#}
+
+sub _max_value {
+    my @values = @_;
+    my @sorted = sort @values;
+    return 0 unless @sorted;
+    return $sorted[-1];
+}
+
+sub _max_label_width {
     my @labels = @_;
 
     return 0 if scalar @labels == 0;
 
     my @lengths = sort map { length $_ } @labels;
-    return $lengths[-1];
+    return $lengths[-1] + 2; # + 2 because of 1 space before and after label
 }
 
 1;
@@ -99,19 +134,19 @@ Term::Vspark - Displays a graph in the terminal
     use Term::Vspark qw/show_graph/;
     binmode STDOUT, ':encoding(UTF-8)';
     print show_graph(
-        values  => [0,1,2,3,4,5],
-        labels  => [0,1,2,3,4,5], # optional
-        max     => 7,             # optional
-        columns => 80,            # optional
+        values  => [0,1,2,3,4,5], # required
+        labels  => [0,1,2,3,4,5],
+        max     => 7,
+        columns => 80,
     );
 
     # The output looks like this:
-    # 0 ▏
-    # 1 ██████████▉
-    # 2 █████████████████████▊
-    # 3 ████████████████████████████████▋
-    # 4 ███████████████████████████████████████████▌
-    # 5 ██████████████████████████████████████████████████████▍
+    # 0 
+    # 1 ███████████
+    # 2 ██████████████████████
+    # 3 █████████████████████████████████
+    # 4 ████████████████████████████████████████████
+    # 5 ███████████████████████████████████████████████████████
 
 
 =head1 DESCRIPTION
@@ -119,9 +154,6 @@ Term::Vspark - Displays a graph in the terminal
 This module displays beautiful graphs in the terminal.  It is a companion to
 Term::Spark but instead of displaying normal sparklines it displays "vertical"
 sparklines.
-
-Note that because the graph is built from utf8 characters, users must setup
-UTF-8 encoding for STDOUT if they wish to print the output.
 
 =head1 METHODS
 
@@ -135,14 +167,17 @@ The 'labels' parameter should be an ArrayRef of strings.  This is optional.
 Each label will be used with the corresponding value.
 
 The 'max' parameter is the maximum value of the graph.  Without this parameter
-you cannot compare graphs because the scaling will change depending on the
-data.  This parameter is optional.
+you cannot compare graphs because the scaling changes depending on the data.
+This parameter is optional.
 
-The 'columns' parameter is the maximum width of the graph.
+The 'columns' parameter is the maximum width of the graph.  This defaults to
+your terminal width or 80 characters -- whichever is smaller.  Set 'columns' to
+'max' if you want to use the full width of your terminal.
 
 =head1 AUTHOR
 
-Gil Gonçalves <lurst@cpan.org>
+Eric Johnson (kablamo)
+Gil Gonçalves <lurst@cpan.org> (original author)
 
 =head1 SEE ALSO
 
--- t/basic.t
+++ t/basic.t
@@ -22,7 +22,8 @@ $expected = <<EOF;
 EOF
 
 $graph = show_graph(
-    values => [0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1, 4],
+    values  => [0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1, 4],
+    columns => 4,
 );
 
 is $graph, $expected, "no labels, 1x scaling, decimals";
@@ -47,6 +48,24 @@ is $graph, $expected, "1x scaling";
 
 $expected = <<EOF;
  0 
+ 1 █████
+ 2 █████
+ 3 █████
+ 4 █████
+ 5 █████
+EOF
+
+$graph = show_graph(
+    values  => [0, 1, 2, 3, 4, 5],
+    labels  => [0, 1, 2, 3, 4, 5],
+    max     => 1,
+    columns => 3 + 5,
+);
+
+is $graph, $expected, "1x scaling";
+
+$expected = <<EOF;
+ 0 
  1 ████
  2 ████████
  3 ████████████
@@ -62,5 +81,41 @@ $graph = show_graph(
 );
 
 is $graph, $expected, "4x scaling";
+
+$expected = <<EOF;
+ 0 
+ 1 ██
+ 2 ████
+ 3 ██████
+ 4 ████████
+ 5 ██████████
+EOF
+
+$graph = show_graph(
+    values  => [0, 1, 2, 3, 4, 5],
+    labels  => [0, 1, 2, 3, 4, 5],
+    max     => 10,
+    columns => 3 + 5 * 4,
+);
+
+is $graph, $expected, "max of 10";
+
+$expected = <<EOF;
+ 0 
+ 1 ███████████
+ 2 ██████████████████████
+ 3 █████████████████████████████████
+ 4 ████████████████████████████████████████████
+ 5 ███████████████████████████████████████████████████████
+EOF
+
+$graph = show_graph(
+        values  => [0,1,2,3,4,5], # required
+        labels  => [0,1,2,3,4,5],
+        max     => 7,
+        columns => 80,
+    );
+
+is $graph, $expected, "example from the synopsis";
 
 done_testing;
